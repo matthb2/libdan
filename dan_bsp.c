fe51cf3feff9a2c635b13c28780fdcd683d88562
Dan Ibanez <dan.a.ibanez@gmail.com>
2012-09-29 15:02:17 -0400

    more work on BSP
    The BSP system now has a cleaner interface,
    implementation seems straightforward.
    stopping to move dan_buffer to a better place.

--- dan_bsp.c
+++ dan_bsp.c
@@ -13,14 +13,17 @@ void dan_bsp_init(dan_bsp* b, int tag, int ibarrier_tag)
         exit(0);
     }
     b->tag = tag;
-    dan_aa_init(&(b->tree));
-    b->message = DAN_MPI_MESSAGE_INIT;
     b->ibarrier->tag = ibarrier_tag;
-    b->state = sending;
 }
 
 static void free_all_takers(dan_aa_tree& t)
 {
+    if (**t == &dan_aa_bottom)
+        return;
+    free_all_takers(&((*t)->left));
+    free_all_takers(&((*t)->right));
+    dan_free(*t);
+    **t = &dan_aa_bottom;
 }
 
 void dan_bsp_begin_superstep(dan_bsp* b)
@@ -30,73 +33,39 @@ void dan_bsp_begin_superstep(dan_bsp* b)
     if (b->state == receiving)
         while (!dan_mpi_ibarrier_done(&(b->ibarrier)));
     free_all_takers(&(b->tree));
+    b->state = sending;
 }
 
-static int int_compare(const void* a, const void* b)
+static bool receiver_less(dan_aa_node* a, dan_aa_node* b)
 {
-    return *((int*)a) - *((int*)b);
+    return ((dan_bsp_receiver*)a)->message.peer
+         < ((dan_bsp_receiver*)b)->message.peer;
 }
 
-void dan_bsp_use_givers(dan_bsp* b, size_t count, int* givers)
+static dan_bsp_receiver* find_receiver(dan_aa_tree t, int peer)
 {
-    if (b->sync_type == dan_use_ibarrier)
-    {
-        dan_free(b->sync.ibarrier);
-    }
-    else if (b->sync_type == dan_use_givers)
-    {
-        dan_free(b->sync.givers);
-    }
-    b->sync.givers = dan_malloc(sizeof(dan_bsp_givers)+count*sizeof(int));
-    b->sync.givers->count = count;
-    for (size_t i=0; i < count; ++i)
-    {
-        b->sync.givers->givers[i] = givers[i];
-    }
-    qsort(b->sync.givers->givers,count,sizeof(int),int_compare);
-    b->sync_type = dan_use_givers;
-}
-
-static bool taker_less(dan_aa_node* a, dan_aa_node* b)
-{
-    return ((dan_bsp_taker*)a)->message.peer < ((dan_bsp_taker*)b)->message.peer;
-}
-
-dan_bsp_taker* dan_bsp_find_taker(dan_bsp* b, int peer)
-{
-    dan_bsp_taker key;
+    dan_bsp_receiver key;
     key->message.peer = peer;
-    return (dan_bsp_taker*) dan_aa_find((dan_aa_node*)&key,b->tree,taker_less);
-}
-
-void dan_bsp_insert_taker(dan_bsp* b, int peer, dan_bsp_taker* result)
-{
-    result->message.peer = peer;
-    dan_aa_node* inserted = dan_aa_insert((dan_aa_node*)result,b->tree,taker_less);
-    if (inserted != (dan_aa_node*)result)
-    {
-        fprintf(stderr,"dan_bsp_insert_taker: there was already a taker with this peer id. use dan_bsp_find_taker to check for it first.\n");
-        exit(0);
-    }
-}
-
-void dan_bsp_add_taker(dan_bsp* b, int peer)
-{
-    dan_bsp_taker* taker = dan_bsp_find_taker(b,peer);
-    if (taker)
-        return;
-    taker = dan_malloc(sizeof(*taker));
-    *taker = DAN_BSP_TAKER_INIT;
-    dan_bsp_insert_taker(b,peer,taker);
+    return (dan_bsp_receiver*) dan_aa_find((dan_aa_node*)&key,t,receiver_less);
 }
 
 void dan_bsp_reserve(dan_bsp* b, int peer, size_t bytes)
 {
-    dan_bsp_taker* taker = dan_bsp_find_taker(b,peer);
-    if (!taker)
+    dan_bsp_receiver* receiver;
+    receiver = find_receiver(b->tree,peer);
+    if (!receiver)
     {
-        fprintf(stderr,"dan_bsp_reserve: there is no taker with this peer id. use dan_bsp_add_taker to add it first.\n");
-        exit(0);
+        receiver = malloc(sizeof(*receiver));
+        dan_aa_insert((dan_aa_node*)receiver,&(b->tree),receiver_less);
     }
+    receiver->message.buffer.size += bytes;
+}
+
+static void allocate(dan_aa_tree t)
+{
+}
+
+void dan_bsp_allocate(dan_bsp* b)
+{
 }
 
--- dan_bsp.h
+++ dan_bsp.h
@@ -24,16 +24,10 @@ typedef struct
 {
     dan_aa_node node;
     dan_mpi_message message;
-} dan_bsp_taker;
+} dan_bsp_receiver;
 
 #define DAN_BSP_TAKER_INIT \
 { .message = DAN_MPI_MESSAGE_INIT }
-
-typedef struct
-{
-    size_t count;
-    int givers[];
-} dan_bsp_givers;
 
 typedef struct
 {
@@ -41,12 +35,15 @@ typedef struct
     dan_aa_tree tree;
     dan_mpi_message received;
     dan_mpi_ibarrier ibarrier;
-    int state;
+    enum { dan_bsp_sending, dan_bsp_receiving } state;
 } dan_bsp;
+
+#define DAN_BSP_INIT \
+{ .tag = 0, .tree = DAN_AA_TREE_INIT, .received = DAN_MPI_MESSAGE_INIT, \
+  .ibarrier = DAN_MPI_IBARRIER_INIT, .state = dan_bsp_sending }
 
 void dan_bsp_init(dan_bsp* b, int tag, int ibarrier_tag);
 void dan_bsp_begin_superstep(dan_bsp* b);
-void dan_bsp_add_taker(dan_bsp* b, int peer);
 void dan_bsp_reserve(dan_bsp* b, int peer, size_t bytes);
 void dan_bsp_allocate(dan_bsp* b);
 void dan_bsp_begin_packing(dan_bsp* b);
