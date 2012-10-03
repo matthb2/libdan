7ffa5fd340160cf123ffff757618bb5d51a4ed0c
Dan Ibanez <dan.a.ibanez@gmail.com>
2012-10-03 12:44:22 -0400

    Adding another phase to test/bsp.c
    The test now runs through one more
    messaging round, just to make sure
    the ibarriers are working as advertised.
    No bugs were found.

--- test/bsp.c
+++ test/bsp.c
@@ -10,12 +10,28 @@ int main(int argc, char** argv)
     dan_bsp_begin_superstep(&bsp);
     int rank = dan_mpi_rank();
     int size = dan_mpi_size();
-    int message = rank;
+    int message = 42;
     int peer = (rank+1)%size;
     dan_bsp_reserve(&bsp,peer,sizeof(message));
     dan_bsp_allocate(&bsp);
     dan_bsp_begin_packing(&bsp);
     int* packed = dan_bsp_pack(&bsp,peer,sizeof(message));
+    *packed = message;
+    dan_bsp_send(&bsp);
+    while (dan_bsp_receive(&bsp))
+    {
+        fprintf(stderr,"%d received %d from %d\n",
+                rank,*((int*)bsp.received.buffer.data),
+                bsp.received.peer);
+    }
+    dan_bsp_begin_superstep(&bsp);
+    fprintf(stderr,"%d step 2\n",rank);
+    message = 128;
+    peer = (rank+(size-1))%size;
+    dan_bsp_reserve(&bsp,peer,sizeof(message));
+    dan_bsp_allocate(&bsp);
+    dan_bsp_begin_packing(&bsp);
+    packed = dan_bsp_pack(&bsp,peer,sizeof(message));
     *packed = message;
     dan_bsp_send(&bsp);
     while (dan_bsp_receive(&bsp))
